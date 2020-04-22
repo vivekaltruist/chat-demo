67f4310b82b453580357062d4479bdd00eb33576
Vivek Kumar <vivkuma@adobe.com>
2020-04-22 12:32:47 +0530

    fix 2

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 5000, () => {
+  console.log('listening on *:5000');
 });
+app.set('port', (process.env.PORT || 5000));
